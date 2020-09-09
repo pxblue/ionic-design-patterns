4fd9243f646dcbf486a70b686c71b7478e5a2ac9
daileytj <daileytj89@gmail.com>
2020-09-09 13:18:19 -0400

    Remove absolute path

--- src/app/pages/list/data-list/data-list.component.ts
+++ src/app/pages/list/data-list/data-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { StateService } from 'src/app/services/state.service';
+import { StateService } from '../../../services/state.service';
 
 @Component({
     selector: 'app-data-list',
