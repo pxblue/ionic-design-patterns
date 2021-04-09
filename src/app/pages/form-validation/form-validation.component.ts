cead8b2e84093e1ed07823462e18da1cf83ad14a
Andrupu <47467951+andrupu@users.noreply.github.com>
2021-04-09 14:06:39 -0400

    Add Input Error State

--- src/app/pages/form-validation/form-validation.component.ts
+++ src/app/pages/form-validation/form-validation.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { FormControl, FormGroupDirective, NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { StateService } from '../../services/state.service';
+
+class CrossFieldErrorMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+      return control.dirty && form.invalid;
+  }
+}
 
 @Component({
   selector: 'app-form-validation',
@@ -6,9 +16,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-validation.component.scss'],
 })
 export class FormValidationComponent implements OnInit {
+  isCollapsed: boolean;
+  basicFrom: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly _drawerService: StateService,
+    private readonly _breakpointObserver: BreakpointObserver,
+    private readonly _formBuilder: FormBuilder
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit(): void { 
+    this._breakpointObserver
+            .observe([Breakpoints.Small, Breakpoints.Handset])
+            .subscribe((state: BreakpointState) => {
+                if (state.matches) {
+                    this.isCollapsed = true;
+                } else {
+                    this.isCollapsed = false;
+                }
+            });
+  }
+
+  toggleMenu(): void {
+    const drawerOpen = this._drawerService.getDrawerOpen();
+    this._drawerService.setDrawerOpen(!drawerOpen);
+  }
 
 }
--- src/app/pages/form-validation/form-validation.module.ts
+++ src/app/pages/form-validation/form-validation.module.ts
@@ -4,6 +4,11 @@ import { FormValidationComponent } from './form-validation.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,13 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   exports: [
     FormValidationComponent]
